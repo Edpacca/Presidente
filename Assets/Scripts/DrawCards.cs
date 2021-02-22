0da3d1e7158c8469b0ad4b9e6239e75b8f5e009f
Edpacca <eddie@pace.me.uk>
2021-02-22 22:41:59 +0000

    initial commit of assets

---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class DrawCards : MonoBehaviour
+{
+    public List<GameObject> Deck;
+    public GameObject HandArea;
+    public List<GameObject> Hand = new List<GameObject>();
+
+    private int _handSize = 5;
+
+    private bool _isHandDealt;
+
+    void Start()
+    {
+    }
+
+    public void StartGameClick()
+    {
+        if (!_isHandDealt)
+        {
+            for (int i = 0; i < _handSize; i++)
+            {
+                int index = GetRandomCard();
+
+                while (Hand.Contains(Deck[index]))
+                {
+                    index = GetRandomCard();
+                }
+
+                GameObject cardInstance = Instantiate(Deck[index], new Vector3(i, 0, 0), Quaternion.identity);
+                cardInstance.transform.SetParent(HandArea.transform);
+                Hand.Add(cardInstance);
+            }
+            _isHandDealt = true;
+        }
+        else
+        {
+            foreach (var card in Hand)
+            {
+                Destroy(card);
+            }
+            
+            _isHandDealt = false;
+        }
+    }
+
+    private int GetRandomCard()
+    {
+        return Random.Range(0, Deck.Count - 1);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class Player
+    {
+        public List<int> Hand;
+
+        public void GiveCard(int index)
+        {
+            Hand.Add(index);
+        }
+    }
+}
