17b095655f2c6e639e4cd28793bf5581c41347d1
Edpacca <eddie@pace.me.uk>
2021-02-23 16:36:04 +0000

    added shuffle and dynamic player position generation methods

--- Assets/Scripts/DrawCards.cs
+++ Assets/Scripts/DrawCards.cs
@@ -2,54 +2,113 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts;
+using UnityEngine.UI;
 
 public class DrawCards : MonoBehaviour
 {
-    public List<GameObject> Deck;
-    public GameObject HandArea;
-    public List<GameObject> Hand = new List<GameObject>();
+    public List<GameObject> GameObjectDeck;
+    public GameObject PlayerHand;
 
-    private int _handSize = 5;
+    public GameObject OpponentHand;
+    public GameObject Canvas;
+
+    private List<GameObject> _playerHandAreas = new List<GameObject>();
+    private List<Card> _cardDeck = new List<Card>();
 
     private bool _isHandDealt;
 
+    public int NumberOfPlayers = 5;
+    public int HandSize;
+
+    private List<Vector3> opponentHandPositions;
+
     void Start()
     {
+        opponentHandPositions = GenerateOpponentHandPositions(NumberOfPlayers - 1, 3.5f, 20);
+        _playerHandAreas.Add(PlayerHand);
+        GenerateOpponentAreas();
+        GenerateCards();
     }
 
     public void StartGameClick()
     {
+        ShuffleCards(_cardDeck);
+
         if (!_isHandDealt)
         {
-            for (int i = 0; i < _handSize; i++)
+            for (int i = 0; i < _cardDeck.Count; i++)
             {
-                int index = GetRandomCard();
-
-                while (Hand.Contains(Deck[index]))
+                foreach (var playerArea in _playerHandAreas)
                 {
-                    index = GetRandomCard();
+                    if (i < _cardDeck.Count)
+                    {
+                        GameObject dealtCard = Instantiate(GameObjectDeck[_cardDeck[i].Index], Vector3.zero, Quaternion.identity);
+                        dealtCard.transform.SetParent(playerArea.transform);
+                        dealtCard.transform.localScale = new Vector3(1, 1, 1);
+                        _cardDeck[i].SetCardGameObject(dealtCard);
+                        i++;
+                    }
                 }
-
-                GameObject cardInstance = Instantiate(Deck[index], new Vector3(i, 0, 0), Quaternion.identity);
-                cardInstance.transform.SetParent(HandArea.transform);
-                Hand.Add(cardInstance);
             }
             _isHandDealt = true;
         }
         else
         {
-            foreach (var card in Hand)
+            foreach (var card in _cardDeck)
             {
-                Destroy(card);
+                Destroy(card.CardGameObject);
             }
             
             _isHandDealt = false;
         }
     }
 
-    private int GetRandomCard()
+    private void GenerateOpponentAreas()
     {
-        return Random.Range(0, Deck.Count - 1);
+        for (int i = 0; i < NumberOfPlayers - 1; i++)
+        {
+            GameObject opponentHandArea = Instantiate(OpponentHand, opponentHandPositions[i], Quaternion.identity);
+            opponentHandArea.transform.SetParent(Canvas.transform);
+            opponentHandArea.transform.localScale = new Vector3(1, 1, 1);
+            _playerHandAreas.Add(opponentHandArea);
+        }
     }
 
+    private List<Vector3> GenerateOpponentHandPositions(int numberOfOpponents, float height, float maxWidth)
+    {
+        List<Vector3> handPositions = new List<Vector3>();
+
+        float xInterval = maxWidth / (numberOfOpponents + 1);
+        float xPosition = -(maxWidth / 2) + xInterval;
+
+        for (int i = 0; i < numberOfOpponents; i++)
+        {
+            handPositions.Add(new Vector3(xPosition, height, 0));
+            xPosition += xInterval;
+        }
+
+        return handPositions;
+    }
+
+    private void GenerateCards()
+    {
+        for (int i = 0; i < GameObjectDeck.Count; i++)
+        {
+            _cardDeck.Add(new Card(i));
+        }
+    }
+
+    private static void ShuffleCards<T>(IList<T> deck)
+    {
+        int count = deck.Count;
+        int last = count - 1;
+
+        for (int i = 0; i < last; i++)
+        {
+            var r = Random.Range(i, count);
+            var buffer = deck[i];
+            deck[i] = deck[r];
+            deck[r] = buffer;
+        }
+    }
 }
