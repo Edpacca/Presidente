0682a0a237d17b401bb53e9838be0db65bbb4b73
Edpacca <eddie@pace.me.uk>
2021-02-24 21:29:26 +0000

    create selectCard script

---
+++ Assets/Scripts/SelectCard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCard : MonoBehaviour
+{
+    public GameObject PlayArea;
+
+    private bool _isSelected;
+    private bool _isPositionStored;
+    private readonly Vector2 _hoverOffset = new Vector2(0, 15);
+    private Vector2 _initialPosition;
+
+    public void OnHover()
+    {
+        if (!_isPositionStored)
+        {
+            _initialPosition = transform.position;
+            _isPositionStored = true;
+        }
+        
+        transform.position = _initialPosition + _hoverOffset;
+    }
+
+    public void ExitHover()
+    {
+        if (!_isSelected)
+            transform.position = _initialPosition;
+    }
+
+    public void OnSelect()
+    {
+        if (!_isSelected)
+            _isSelected = true;
+        else
+        {
+            _isSelected = false;
+            ExitHover();
+        }
+    }
+}
