3acda719a8042747fc9f85749236e8838af8bdf0
Edpacca <eddie@pace.me.uk>
2021-02-24 21:31:04 +0000

    Moved shuffle function to static class CardManager

---
+++ Assets/Classes/CardManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Classes
+{
+    public static class CardManager
+    {
+        public static void ShuffleCards<T>(IList<T> deck)
+        {
+            int count = deck.Count;
+            int last = count - 1;
+
+            for (int i = 0; i < last; i++)
+            {
+                var r = Random.Range(i, count);
+                var buffer = deck[i];
+                deck[i] = deck[r];
+                deck[r] = buffer;
+            }
+        }
+    }
+}
