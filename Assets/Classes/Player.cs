afcb4ac3c258c126097e46a57a185277495ddf58
Edpacca <eddie@pace.me.uk>
2021-02-24 21:31:30 +0000

    create Player class

---
+++ Assets/Classes/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Classes
+{
+    public class Player
+    {
+        public List<Card> Hand { get; }
+        public int Score { get; set; }
+
+
+        public void DealCard(Card card)
+        {
+            Hand.Add(card);
+        }
+    }
+}
