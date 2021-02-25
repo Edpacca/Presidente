c9a277e68aa1e9b508ee1982b87890fae13da302
Edpacca <eddie@pace.me.uk>
2021-02-25 16:25:48 +0000

    create playmanager class

---
+++ Assets/Classes/PlayManager.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Classes
+{
+    public class PlayManager
+    {
+        public int ActivePlayer { get; set; } = 0;
+        public int NumberOfPlayers { get; }
+
+        public PlayManager(int numberOfPlayers)
+        {
+            NumberOfPlayers = numberOfPlayers;
+        }
+
+        public void Update()
+        {
+
+        }
+
+
+        public void TryPlayCards()
+        {
+
+        }
+
+        private void PlayCard(Card card)
+        {
+
+        }
+
+        private void PlayCards(List<Card> cards)
+        {
+
+        }
+
+    }
+}
