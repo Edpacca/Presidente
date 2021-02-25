8105222d2d3ba96ea503ec30de3a51191e17014b
Edpacca <eddie@pace.me.uk>
2021-02-25 16:21:39 +0000

    removed function from player class

--- Assets/Classes/Player.cs
+++ Assets/Classes/Player.cs
@@ -8,13 +8,32 @@ namespace Assets.Classes
 {
     public class Player
     {
-        public List<Card> Hand { get; }
+        public List<Card> Hand { get; } = new List<Card>();
+        public List<Card> SelectedCards { get; set; } = new List<Card>();
         public int Score { get; set; }
-
 
         public void DealCard(Card card)
         {
             Hand.Add(card);
+        }
+
+        public void PlayCards()
+        {
+            foreach (var card in Hand)
+            {
+                if (SelectedCards.Contains(card))
+                    Hand.Remove(card);
+            }
+
+            SelectedCards.Clear();
+        }
+
+        public void SetSelectedCards()
+        {
+            foreach (var card in Hand.Where(c => c.CardGameObject.GetComponent<SelectCard>().IsSelected))
+            {
+                SelectedCards.Add(card);
+            }
         }
     }
 }
