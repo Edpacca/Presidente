0aafba20e76c5c7a3c3f003d75266c6b807c7b0d
Edpacca <eddie@pace.me.uk>
2021-02-25 16:27:34 +0000

    create GameSetup and GameSettings scriptable objects

---
+++ Assets/ScriptableObjects/GameSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "Settings/GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    private const int singleDeckSize = 52;
+    public int numberOfPlayers = 5;
+    public int numberOfDecks = 1;
+    public int DeckSize { get => singleDeckSize * numberOfDecks; }
+
+    public int DeckIndex(int index)
+    {
+        return index % singleDeckSize;
+    }
+
+    public Color32 activePlayColour = new Color(1.000f, 0.600f, 0.000f, 0.250f);
+    public Color32 defaultAreaColour = new Color(1.000f, 1.000f, 1.000f, 0.250f);
+}
