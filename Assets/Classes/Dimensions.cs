0eeb7c03f41a4e4527333fb15e0566eae290fd8e
Edpacca <eddie@pace.me.uk>
2021-02-25 16:24:00 +0000

    utility class for Vector2 position

---
+++ Assets/Classes/Dimensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Classes
+{
+    public static class Dimensions
+    {
+        public static Vector2 CornerPosition(GameObject gameObject)
+        {
+            RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+            Vector2 position = gameObject.transform.position;
+            return rectTransform.offsetMin;
+        }
+    }
+}
