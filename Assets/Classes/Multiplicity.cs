4c630afbe2274e2cf5ed519f72b3a187d4f63f49
Edpacca <eddie@pace.me.uk>
2021-02-25 16:24:49 +0000

    create enumeration for game type

---
+++ Assets/Classes/Multiplicity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Classes
+{
+    public enum Multiplicity
+    {
+        Singles = 1,
+        Doubles = 2,
+        Triples = 3,
+        Quadruples = 4,
+    }
+}
