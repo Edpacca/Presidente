be4ff054cbf9b0fb59b4c08185718a76592e36a3
Edpacca <eddie@pace.me.uk>
2021-02-27 23:02:58 +0000

    create card glow on select script

---
+++ Assets/Scripts/Cards/CardGlow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardGlow : MonoBehaviour
+{
+    public Image cardGlow;
+
+    void Start()
+    {
+        cardGlow.enabled = false;        
+    }
+
+    public void OnEnterHover()
+    {
+        cardGlow.enabled = true;
+    }
+
+    public void OnExitHover()
+    {
+        cardGlow.enabled = false;
+    }
+}
