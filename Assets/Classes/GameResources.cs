d32cb8ed52d9a34c6fc4b50ffe1f8e06e48dc3b3
Edpacca <eddie@pace.me.uk>
2021-03-01 16:34:46 +0000

    static class to contain constant parameters like colours etc

---
+++ Assets/Classes/GameResources.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    public static class GameResources
+    {
+        public static Color32 alphaWhite = new Color(1.0f, 1.0f, 1.0f, 0.31f);
+        public static Color32 alphaPink = new Color(1.0f, 0.0f, 1.0f, 0.31f);
+    }
+}
+
+
+
+
