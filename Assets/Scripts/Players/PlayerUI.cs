98ba7e978623be2563f97461121560b7b375d912
Edpacca <eddie@pace.me.uk>
2021-03-01 16:34:23 +0000

    create player UI script

---
+++ Assets/Scripts/Players/PlayerUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    public Text cardsInHand;
+    public Text playerName;
+    private GameObject _handArea;
+
+    public void Initialise(GameObject handArea)
+    {
+        _handArea = handArea;
+    }
+
+    public void UpdateCardsInHand(int number)
+    {
+        cardsInHand.text = number.ToString();
+    }
+
+    public void SetName(string name)
+    {
+        playerName.text = name;
+    }
+
+}
