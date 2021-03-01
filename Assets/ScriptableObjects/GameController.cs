e0324524f9c44bb6ab09c6a094682e504558c283
Edpacca <eddie@pace.me.uk>
2021-03-01 16:36:34 +0000

    updated player control in gameController

--- Assets/ScriptableObjects/GameController.cs
+++ Assets/ScriptableObjects/GameController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName ="GameController", menuName ="Controllers/GameController")]
 public class GameController : ScriptableObject
 {
     public GameSettings gameSettings;
-    protected List<Player> players; 
+    protected List<Player> players;
+    public int ActivePlayer;
+    public int numberOfPlayers;
 
     public void AssignPlayers(List<Player> newPlayers)
     {
@@ -14,13 +17,37 @@ public class GameController : ScriptableObject
             players.Clear();
 
         players = newPlayers;
+        numberOfPlayers = players.Count;
     }
 
-    public void DealCardToPlayer(int playerId, Card card)
+    public void DealCardToPlayer(int playerId, GameObject card)
     {
         if (playerId >= players.Count)
             return;
 
         players[playerId].DealCard(card);
+    }
+
+    public void StartPlay()
+    {
+        Player startPlayer = players.SingleOrDefault(p => p.isActivePlayer);
+        ActivePlayer = players.IndexOf(startPlayer);
+    }
+
+    public void TurnTaken()
+    {
+        players[ActivePlayer].UnsetActivePlayer();
+        NextTurn();
+        players[ActivePlayer].SetActivePlayer();
+    }
+
+    private void NextTurn()
+    {
+        ActivePlayer = (ActivePlayer + 1) % numberOfPlayers;
+    }
+
+    private void CheckForPresident(Player player)
+    {
+
     }
 }
