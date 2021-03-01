c8927b936893ecac031325996dc026cf2343cec8
Edpacca <eddie@pace.me.uk>
2021-03-01 16:35:56 +0000

    playAreaScript handles playstate of last played card

--- Assets/Scripts/PlayAreaScript.cs
+++ Assets/Scripts/PlayAreaScript.cs
@@ -11,6 +11,7 @@ public class PlayAreaScript : MonoBehaviour
 
     public Image image;
     public Text lastPlayedValueText;
+    public GameController gameController;
 
     private void Update()
     {
@@ -32,6 +33,7 @@ public class PlayAreaScript : MonoBehaviour
     {
         SetNewSprite(card.cardSprite);
         lastPlayedValue = card.Value;
+        gameController.TurnTaken();
     }
 
     public void UpdateText()
