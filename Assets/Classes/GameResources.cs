1da4b3ff0b05618cdb09c719554b177c2f9cedf9
Edpacca <eddie@pace.me.uk>
2021-03-08 14:04:42 +0000

    added colours

--- Assets/Classes/GameResources.cs
+++ Assets/Classes/GameResources.cs
@@ -6,11 +6,12 @@ namespace Assets
 {
     public static class GameResources
     {
-        public static Color32 alphaWhite = new Color(1.0f, 1.0f, 1.0f, 0.31f);
-        public static Color32 alphaPink = new Color(1.0f, 0.0f, 1.0f, 0.31f);
+        public static Color32 alphaWhite30 = new Color(1.0f, 1.0f, 1.0f, 0.30f);
+        public static Color32 alphaWhite40 = new Color(1.0f, 1.0f, 1.0f, 0.40f);
+        public static Color32 alphaPink = new Color(1.0f, 0.0f, 1.0f, 0.30f);
+        public static Color32 playerGold = new Color(1.0f, 0.67f, 0.0f, 1.0f);
+        public static Color32 playerRed = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        public static Color32 playerGreen = new Color32(0, 255, 92, 255);
+        public static Color32 fadedCard = new Color(0.55f, 0.55f, 0.55f, 1.0f);
     }
 }
-
-
-
-
