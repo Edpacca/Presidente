86b2be946588b9da60554464c833bc4290d66441
Edpacca <eddie@pace.me.uk>
2021-03-08 14:06:04 +0000

    basic logic for button scripts

---
+++ Assets/Scripts/GameStatusText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets;
+using UnityEngine.UI;
+
+public class GameStatusText : MonoBehaviour
+{
+    public GameLoop gameLoop;
+    public Text statusText;
+    private string newGame = "New Game!";
+    private string gameOver = "Game Over!";
+
+    void Update()
+    {
+        if (gameLoop.gameState == GameState.inPlay)
+            gameObject.SetActive(false);
+
+        if (gameLoop.gameState == GameState.gameOver)
+        {
+            gameObject.SetActive(true);
+            statusText.text = gameOver;
+        }
+    }
+}
---
+++ Assets/Scripts/PassButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets;
+
+public class PassButtonScript : MonoBehaviour
+{
+    public GameLoop gameLoop;
+    public GameController gameController;
+
+    public void SetActive(bool active)
+    {
+        gameObject.SetActive(active);
+    }
+}
