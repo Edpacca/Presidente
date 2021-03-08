ac67b06247fa78c10942977434284cc92695b087
Edpacca <eddie@pace.me.uk>
2021-03-08 14:01:41 +0000

    improve control of player state

--- Assets/ScriptableObjects/Player.cs
+++ Assets/ScriptableObjects/Player.cs
@@ -8,11 +8,16 @@ using System;
 [CreateAssetMenu(fileName ="newPlayer", menuName ="Objects/Player")]
 public class Player : ScriptableObject
 {
+
     public List<GameObject> hand = new List<GameObject>();
-    private GameObject _handArea;
     public bool isActivePlayer;
+    public bool isOutOfGame;
     public GameObject playerUi;
-    private PlayerUI UiScript;
+    private PlayerUI _uiScript;
+    private GameObject _handArea;
+    private int _highestValueInPlay;
+    public bool isPresident;
+    public bool isScum;
 
     public void DealCard(GameObject card)
     {
@@ -24,46 +29,68 @@ public class Player : ScriptableObject
     {
         _handArea = handArea;
         playerUi = playerUiObject;
-        UiScript = playerUi.GetComponent<PlayerUI>();
+        _uiScript = playerUi.GetComponent<PlayerUI>();
     }
 
     public void UpdateHandSize()
     {
-        UiScript.UpdateCardsInHand(hand.Count);
+        int handSize = hand.Count;
+        _uiScript.UpdateCardsInHand(handSize);
+
+        if (handSize == 0)
+            SetOutOfGame();
     }
 
-    public void CheckIfFirstPlayer(GameObject card)
+    private void CheckIfFirstPlayer(GameObject card)
     {
         if (GetCard(card).Index == GameSettings.startCardIndex)
         {
             isActivePlayer = true;
-            GetScript(card).isPlayable = true;
+            GetScript(card).SetPlayable(true);
             _handArea.GetComponent<Image>().color = GameResources.alphaPink;
         }
     }
 
-    public void CheckForPlayedCards()
+    public void SetStartHand()
     {
         foreach (var card in hand)
         {
+            if (GetCard(card).Value != 2)
+                GetScript(card).MarkUnplayable();
+        }
+    }
+
+    private void CheckForPlayedCards()
+    {
+        List<GameObject> playedCards = new List<GameObject>();
+
+        foreach (var card in hand)
+        {
             if (GetScript(card).isPlayed == true)
-                hand.Remove(card);
+                playedCards.Add(card);
+        }
+
+        foreach (var card in playedCards)
+        {
+            hand.Remove(card);
         }
 
         UpdateHandSize();
     }
 
-    public void SetActivePlayer()
+    public void SetActivePlayer(int highestCardInPlay)
     {
+        _highestValueInPlay = highestCardInPlay;
         isActivePlayer = true;
         _handArea.GetComponent<Image>().color = GameResources.alphaPink;
+        _uiScript.PassTurn(false);
         ActivateHand();
     }
 
     public void UnsetActivePlayer()
     {
         isActivePlayer = false;
-        _handArea.GetComponent<Image>().color = GameResources.alphaWhite;
+        _handArea.GetComponent<Image>().color = GameResources.alphaWhite30;
         CheckForPlayedCards();
         DeactivateHand(hand);
     }
@@ -71,13 +98,54 @@ public class Player : ScriptableObject
     private void ActivateHand()
     {
         foreach (var card in hand)
-            GetScript(card).isPlayable = true;
+        {
+            bool isPlayable = false;
+
+            if (GetCard(card).Value > _highestValueInPlay)
+                isPlayable = true;
+
+            GetScript(card).SetPlayable(isPlayable);
+        }
     }
 
     private void DeactivateHand(List<GameObject> playerHand)
     {
         foreach (var card in playerHand)
-            GetScript(card).isPlayable = false;
+        {
+            GetScript(card).ResetCard();
+        }
+    }
+
+    public void DEBUG_SplurgeHand()
+    {
+        foreach (var card in hand)
+        {
+            card.transform.parent = null;
+        }
+
+        hand.Clear();
+    }
+
+    public void PassTurn()
+    {
+        _uiScript.PassTurn(true);
+    }
+
+    public void Reset()
+    {
+        _uiScript.PassTurn(false);
+        _highestValueInPlay = 0;
+
+        if (isActivePlayer)
+        {
+            ActivateHand();
+        }
+    }
+
+    public void SetOutOfGame()
+    {
+        isOutOfGame = true;
+        _uiScript.SetOutOfGame();
     }
 
     private Card GetCard(GameObject cardObject) => cardObject.GetComponent<CardScript>().card;
