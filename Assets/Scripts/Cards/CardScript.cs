7fa4afde09af8e398a98a6311dc36ce6e4208a8f
Edpacca <eddie@pace.me.uk>
2021-03-08 13:58:45 +0000

    Set card colour within script

--- Assets/Scripts/Cards/CardScript.cs
+++ Assets/Scripts/Cards/CardScript.cs
@@ -1,10 +1,38 @@
-﻿using System.Collections;
+﻿using Assets;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardScript : MonoBehaviour
 {
     public Card card;
+    public Image image;
     public bool isPlayable = false;
     public bool isPlayed = false;
+
+    public void SetPlayable(bool playable)
+    {
+        isPlayable = playable;
+
+        if (isPlayable)
+        {
+            image.color = Color.white;
+        }
+        else
+        {
+            MarkUnplayable();
+        }
+    }
+
+    public void ResetCard()
+    {
+        image.color = Color.white;
+        isPlayable = false;
+    }
+
+    public void MarkUnplayable()
+    {
+        image.color = GameResources.fadedCard;
+    }
 }
