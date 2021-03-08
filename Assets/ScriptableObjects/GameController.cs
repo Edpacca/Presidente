255f957b892341ac7b0910c52429d5d9089ceff1
Edpacca <eddie@pace.me.uk>
2021-03-08 14:03:48 +0000

    developed event based logic of game

--- Assets/ScriptableObjects/GameController.cs
+++ Assets/ScriptableObjects/GameController.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Assets;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName ="GameController", menuName ="Controllers/GameController")]
 public class GameController : ScriptableObject
 {
     public GameSettings gameSettings;
+    public GameLoop gameLoop;
+
+    private PlayAreaScript playAreaScript;
+    private PassButtonScript passButtonScript;
+
     protected List<Player> players;
     public int ActivePlayer;
     public int numberOfPlayers;
+    private int numberOfPassedPlayers;
 
     public void AssignPlayers(List<Player> newPlayers)
     {
@@ -18,6 +26,7 @@ public class GameController : ScriptableObject
 
         players = newPlayers;
         numberOfPlayers = players.Count;
+        GetGameObjectReferences();
     }
 
     public void DealCardToPlayer(int playerId, GameObject card)
@@ -32,22 +41,78 @@ public class GameController : ScriptableObject
     {
         Player startPlayer = players.SingleOrDefault(p => p.isActivePlayer);
         ActivePlayer = players.IndexOf(startPlayer);
+        startPlayer.SetStartHand();
+        gameLoop.gameState = GameState.newGame;
+        passButtonScript.SetActive(false);
     }
 
-    public void TurnTaken()
+    private void TurnTaken()
     {
         players[ActivePlayer].UnsetActivePlayer();
         NextTurn();
-        players[ActivePlayer].SetActivePlayer();
+        players[ActivePlayer].SetActivePlayer(playAreaScript.lastPlayedValue);
     }
 
     private void NextTurn()
     {
         ActivePlayer = (ActivePlayer + 1) % numberOfPlayers;
+        if (players[ActivePlayer].isOutOfGame)
+            NextTurn();
     }
 
-    private void CheckForPresident(Player player)
+    public void PassTurn()
     {
+        players[ActivePlayer].PassTurn();
+        numberOfPassedPlayers++;
+        CheckForBurn();
+        TurnTaken();
+    }
 
+    private void CheckForBurn()
+    {
+        if (numberOfPassedPlayers == numberOfPlayers - 1)
+            playAreaScript.BurnCardsInPlay();
+    }
+
+    public void PlayMade()
+    {
+        if (gameLoop.gameState != GameState.inPlay)
+        {
+            gameLoop.gameState = GameState.inPlay;
+            passButtonScript.SetActive(true);
+        }
+
+        numberOfPassedPlayers = players.Where(p => p.isOutOfGame).Count();
+        TurnTaken();
+        CheckForEndOfGame();
+    }
+
+    private void GetGameObjectReferences()
+    {
+        playAreaScript = GameObject.Find("PlayArea").GetComponent<PlayAreaScript>();
+        passButtonScript = GameObject.Find("PassButton").GetComponent<PassButtonScript>();
+    }
+
+    public void WinningPlayMade()
+    {
+        gameLoop.gameState = GameState.newRound;
+        passButtonScript.SetActive(false);
+
+        foreach (var player in players)
+        {
+            player.Reset();
+        }
+    }
+
+    public void DEBUG_Splurge()
+    {
+        players[ActivePlayer].DEBUG_SplurgeHand();
+        PlayMade();
+    }
+
+    public void CheckForEndOfGame()
+    {
+        if (players.Where(p => p.isOutOfGame).Count() == numberOfPlayers - 1)
+            gameLoop.gameState = GameState.gameOver;
     }
 }
