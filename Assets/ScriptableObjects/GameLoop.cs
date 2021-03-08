c7c696a9ebe3bb2241d82c0274be893856111f5e
Edpacca <eddie@pace.me.uk>
2021-03-08 14:02:47 +0000

    remove unnecessary switch case

--- Assets/ScriptableObjects/GameLoop.cs
+++ Assets/ScriptableObjects/GameLoop.cs
@@ -2,25 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName ="GameLoop", menuName ="Controllers/GameLoop")]
 public class GameLoop : ScriptableObject
 {
+    public Text statusText;
     public GameState gameState = GameState.inMenu;
-
-    public void GameFlow(GameState gameState)
-    {
-        switch(gameState)
-        {
-            case GameState.newGame:
-                {
-                    break;
-                }
-            case GameState.boardLoaded:
-                {
-                    break;
-                }
-        }
-    }
 
 }
